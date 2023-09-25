a07c3f3516db91649fd8b06b9646da4a0d1fc27a
Daniel Keefe <dfk@umn.edu>
2023-09-25 09:01:08 -0500

    added soln code

--- index.ts
+++
@@ -1,9 +0,0 @@
-/** CSci-4611 Example Code
- * Copyright 2023+ Regents of the University of Minnesota
- * Please do not distribute beyond the CSci-4611 course
- */
-
-import { ExampleApp } from './ExampleApp'
-
-const app = new ExampleApp();
-app.start();
